b01c979c3eed77912fa226c65f0198921aa6c47e
EmmanuelNativel <emmanuel.nativel@icloud.com>
2019-04-10 22:29:37 +0400

    Correction bug écran noir lorsqu'on lance le jeu. Solution : passage en mode paysage de la fenêtre des menus juste avant de lancer le jeu

--- app/src/main/java/com/nativele/survivor/Menu.java
+++ app/src/main/java/com/nativele/survivor/Menu.java
@@ -5,21 +5,25 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class Menu extends Activity {
 
-    Button btMenuJouer;
-    Button btMenuScore;
+    //Button btMenuJouer;
+    //Button btMenuScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        setContentView(R.layout.menu);
 
-        btMenuJouer= (Button)findViewById(R.id.BtJouer);
-        btMenuScore= (Button)findViewById(R.id.BtScore);
+        setContentView(R.layout.menu);
+        /*
+        btMenuJouer = findViewById(R.id.BtJouer);
+        btMenuScore = findViewById(R.id.BtScore);
+
 
         btMenuJouer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +39,18 @@ public class Menu extends Activity {
                 Intent intent = new Intent(v.getContext(), Score.class);
                 startActivity(intent);
             }
-        });
+        }); */
+    }
+
+    public void lunchGameScene(android.view.View view){
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        startActivity(intent);
+    }
+
+    public void lunchScoreSceen(android.view.View view){
+        Intent intent = new Intent(view.getContext(), Score.class);
+        startActivity(intent);
     }
 
 }
