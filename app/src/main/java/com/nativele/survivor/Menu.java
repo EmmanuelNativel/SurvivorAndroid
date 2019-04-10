9d24df5a79f17c9969a4215dd5347f67999cca8d
EmmanuelNativel <emmanuel.nativel@icloud.com>
2019-04-10 22:40:18 +0400

    Correction bug écran noir au lancement du jeu depuis le menu en mode portrait. Solution : passer la fenêtre menu en mode paysage juste avant de lancer la scène de jeu.

--- app/src/main/java/com/nativele/survivor/Menu.java
+++ app/src/main/java/com/nativele/survivor/Menu.java
@@ -4,14 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-
 public class Menu extends Activity {
 
-    Button btMenuJouer;
-    Button btMenuScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +14,19 @@ public class Menu extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.menu);
-
-        btMenuJouer= (Button)findViewById(R.id.BtJouer);
-        btMenuScore= (Button)findViewById(R.id.BtScore);
-
-        btMenuJouer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btMenuScore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), Score.class);
-                startActivity(intent);
-            }
-        });
     }
+
+    public void lunchGameScene(android.view.View view){
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        startActivity(intent);
+    }
+
+    public void lunchScoreScreen(android.view.View view){
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
